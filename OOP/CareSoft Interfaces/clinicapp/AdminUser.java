cb4dac893db160760dba35c824910bedac0350af
mounir-said <mou.sai.ms3@gmail.com>
2024-07-20 13:39:40 +0100

    java

---
+++ OOP/CareSoft Interfaces/clinicapp/AdminUser.java
@@ -0,0 +1,31 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
+	private Integer employeeID;
+    private String role;
+    private ArrayList<String> securityIncidents;
+    
+    // TO DO: Implement a constructor that takes an ID and a role
+    // TO DO: Implement HIPAACompliantUser!
+    // TO DO: Implement HIPAACompliantAdmin!
+    
+    public void newIncident(String notes) {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+            new Date(), this.id, notes
+        );
+        securityIncidents.add(report);
+    }
+    public void authIncident() {
+        String report = String.format(
+            "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+            new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+        );
+        securityIncidents.add(report);
+    }
+    
+    // TO DO: Setters & Getters
+}
---
+++ OOP/CareSoft Interfaces/clinicapp/Physician.java
@@ -0,0 +1,22 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+	private ArrayList<String> patientNotes;
+	
+	// TO DO: Constructor that takes an IDcopy
+    // TO DO: Implement HIPAACompliantUser!
+	
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+	
+    // TO DO: Setters & Getters
+}
